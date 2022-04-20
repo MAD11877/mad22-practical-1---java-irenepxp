cefc57e9eac96e6dd8ba5ba639d74943980e0818
irenepxp <94032141+irenepxp@users.noreply.github.com>
2022-04-20 17:33:01 +0800

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -1,11 +1,9 @@
-public class Question1
+public class Main
 {
   public static void main(String[] args)
   {
-    /**
-     * Print "Hello World!"
-     * 
-     * Hint: System.out.println(...)
-     */
+     
+    System.out.println("Hello World!");
+     
   }
 }
